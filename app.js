5212857a987b8c0ca1f11163b0979a368b372f31
Chris Hamm <chris.m.hamm@gmail.com>
2015-09-02 14:35:23 -0400

    attempting to store information in a factory for use between views/controllers

--- app.js
+++ app.js
@@ -2,35 +2,56 @@ angular.module('calcApp', ['ngMessages', 'ngRoute'])
     .config(function($routeProvider) {
         $routeProvider.when('/', {
             templateUrl: './home.html',
-            controller: 'HomeCtrl'
         })
         .when('/meal', {
             templateUrl: './meal.html',
-            controller: 'calcCtrl'
+            controller: 'mealCtrl'
         })
         .when('/earnings', {
             templateUrl: './earnings.html',
-            controller: 'calcCtrl'
+            controller: 'earningsCtrl'
+        })
+        .when('/error', {
+            templateUrl: './home.html'
         })
     })
         
+    .factory('data', function() {
+        return  {  
+            values: {
+                tax: 6.25,
+                menuPrice: 0, 
+                tip: 0, 
+                subTotal: 0,
+                total: 0,
+                tipTotal: 0,
+                mealTotal: 0,
+                tipAvg: 0
+        }
+    }
+})
+            
         
-        
-    .controller('calcCtrl', function($scope) {
-        
+    .controller('mealCtrl', function($scope, data) {
+        var data = data.values;
+    
         $scope.tax = 6.25;
         $scope.subtotal = 0;
         $scope.tipDisplay = 0;
         $scope.total = 0;
-        $scope.tipTotal = 0;
-        $scope.mealTotal = 0;
-        $scope.tipAvg = 0;
-        $scope.tip = 0;
+        
+        
         
         // ng-onclick function
         $scope.customerPrice = function(menuPrice, tax, tip) {
             // Subtotal (Price + Tax)
+            data.menuPrice = menuPrice;
+console.log(data.menuPrice);
+            data.tax = tax;
+console.log(data.tax);
             $scope.subtotal = menuPrice + (menuPrice * (tax/100));
+            data.subTotal = $scope.subtotal;
+console.log(data.subTotal);
             
             // Handle no tip
             if ($scope.tip == "") {
@@ -39,25 +60,42 @@ angular.module('calcApp', ['ngMessages', 'ngRoute'])
             
             // Tip (Price * Tax %)
             $scope.tipDisplay = menuPrice * (tip/100);
+            data.tip = $scope.tipDisplay;
+console.log(data.tip);
             
             // Total (Subtotal + Tip)
             $scope.total = $scope.subtotal + $scope.tipDisplay;
-            
+            data.total = $scope.total;
+console.log(data.total);
+        }
+})
+    .controller('earningsCtrl', function($scope, data) {
+            var data = data.values;
+    
+            $scope.tipTotal = data.tipTotal;
+            $scope.mealTotal = 0;
+            $scope.tipAvg = 0;
+            $scope.tip = 0;
+    
             // Session Tip Total
-            $scope.tipTotal = $scope.tipTotal + $scope.tipDisplay;
+            $scope.tipTotal = data.tipTotal + data.tipDisplay;
+            /*root.tipTotal = $scope.tipTotal;*/
             
             // Session Meal Total
             $scope.mealTotal = $scope.mealTotal + 1;
+            data.mealTotal = $scope.mealTotal;
             
             // Session Tip Average
             $scope.tipAvg = $scope.tipTotal / $scope.mealTotal;
+            data.tipAvg = $scope.tipAvg;
             
             // Reset Inputs
             $scope.menuPrice = "";
             $scope.tip = "";
-            $scope.detailsInfo.$setPristine();
+            /*$scope.detailsInfo.$setPristine();*/
                         
-        };
+        
+    
         
         $scope.reset = function() {
             $scope.subtotal = 0;
@@ -66,12 +104,12 @@ angular.module('calcApp', ['ngMessages', 'ngRoute'])
             $scope.tax = 6.25;
             $scope.menuPrice = "";
             $scope.tip = "";
-            $scope.detailsInfo.$setPristine();
+/*            $scope.detailsInfo.$setPristine();*/
         };
         
     });
 
-
+    
 
 
                                
